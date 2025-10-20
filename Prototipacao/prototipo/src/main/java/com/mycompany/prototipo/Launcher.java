a8d617e8acaaa53c39d1868951b594848f557c5a
ArthurHCaron <arthurhcaron@gmail.com>
2025-10-20 16:11:42 -0300

    Tentativa de criar um .jar executável

---
+++ Prototipacao/prototipo/src/main/java/com/mycompany/prototipo/Launcher.java
@@ -0,0 +1,11 @@
+package com.mycompany.prototipo;
+
+/**
+ *
+ * @author arthur
+ */
+public class Launcher {
+    public static void main(String args[]){
+        Main.main(args);
+    }
+}
--- Prototipacao/prototipo/src/main/java/com/mycompany/prototipo/LoginController.java
+++ Prototipacao/prototipo/src/main/java/com/mycompany/prototipo/LoginController.java
@@ -13,7 +13,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
+import javafx.scene.control.Button; 
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
